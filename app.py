be6199df38c621ed49b23ab766c68cf91169d6d1
CourtneyV2025 <courtney.valis2025@gbwl.org>
2025-02-27 19:25:24 +0000

    made the thang

---
+++ app.py
@@ -0,0 +1,11 @@
+# Your app should store, display, and modify data from an SQLite database. Library System: A database of books with borrowing and returning functionality. 
+
+from flask import Flask, render_template, request, redirect 
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timezone 
+
+# Create Flask app instance 
+app = Flask(__name__) 
+
+db = SQLAlchemy 
+
